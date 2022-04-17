b49726b3e61bd331cd9290d59c39e613d17a6f0e
Samy Ali <samy.alisanchez91@gmail.com>
2022-04-17 10:39:48 +0200

    changes

--- array_exercises.js
+++ array_exercises.js
@@ -6,14 +6,14 @@
 // Example:
 // fill(3, 'a'); // expected output => ['a', 'a', 'a']
 function manyTimes(string, times) {
-    const repeatString = [];
-  
-    for (let i = 0; i < times; i++) {
-      repeatString.push(string);
-    }
-    return repeatString;
+  const repeatString = [];
+
+  for (let i = 0; i < times; i++) {
+    repeatString.push(string);
   }
-  console.log(manyTimes("samy", 4));
+  return repeatString;
+}
+console.log(manyTimes("samy", 4));
 
 // Exercise 2:
 // Write a function that takes an array as the only argument and reverts it. Avoid using array.prototype.reverse().
@@ -21,33 +21,49 @@ function manyTimes(string, times) {
 // reverse([1,2,3]); // expected output => [3,2,1]
 
 function reverso(arrRevers) {
-    const readReverse = [];
-    for (let i = arrRevers.length - 1; i >= 0; i--) {
-      readReverse.push(arrRevers[i]);
-    }
-    return readReverse;
+  const readReverse = [];
+  for (let i = arrRevers.length - 1; i >= 0; i--) {
+    readReverse.push(arrRevers[i]);
   }
-  
-  console.log(reverso([5, 7, 8]));
-  
+  return readReverse;
+}
+
+console.log(reverso([5, 7, 8]));
+
 // Exercise 3:
 // Write a function that clears an array from all unnecessary elements (false, undefined, empty strings, 0, null).
 // Example:
 // compact([0, 1, false, 2, undefined, '', 3, null]); // expected output => [1,2,3]
 
-const dirtyArr = [0, 1, false, 2, undefined, '', 3, null];
-
-function clear (arrClear){
 
 
-  return dirtyArr
+function clearArray(arr) {
+
+  const array = arr.filter((value) => {
+    return (
+      (value !== false) & (value !== 0) &&
+      value !== undefined &&
+      value !== "" &&
+      value !== null
+    );
+  });
+  return array;
 }
+console.log(clearArray([0, 1, false, 2, undefined, '', 3, null]));
+
+/*const dirtyArr = [0, 1, false, 2, undefined, "", 3, null];
+
+function arrClear(arr) {
+  const clearArr = arr.filter(item => !isNaN(parseInt(item)))
+
+  return clearArr;
+  */
+  
 
 // Exercise 4:
 // Write a function that takes as an argument an array of arrays and returns an object composed of key-value pairs.
-// Example: 
+// Example:
 // formPairs([['a', 1],['b', 2]]); // expected output => { a: 1, b: 2}
-
 
 // Exercise 5:
 // Write a function that takes an array as a first argument and then two values as the second and third argument.
@@ -55,26 +71,25 @@ function clear (arrClear){
 // Example:
 // without([1,2,3,1,2], 1, 2); // expected output => [3]
 
-
 // Exercise 6:
 // Write a function that takes two arrays as arguments and compares if they are identical.
-// Example: 
+// Example:
 // isEqual([1, 2, 3, 4],[1, 2, 3, 4]) // expected output => true
 // isEqual([1, 2, 3, 4],[1, 2, 3, 4, 5]) // expected output => false
 
 function isEqual(firstArray, secondArray) {
-    for (let i = 0; i < firstArray.length; i++) {
-      if (firstArray.length !== secondArray.length) return false;
-  
-      if (firstArray[i] !== secondArray[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-  console.log(isEqual([1, 2, 3, 4], [1, 2, 3, 4]));
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray.length !== secondArray.length) return false;
 
-// Bonus – Exercise 7: 
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(isEqual([1, 2, 3, 4], [1, 2, 3, 4]));
+
+// Bonus – Exercise 7:
 // Write a function that takes an array as the first argument and a number as the second argument.
 // It should return the array split into parts of the determined size.
 // Example:
